83db70ed1830415e639e2d0196397e2a72974b02
mdsiamul <mdsiamulsuratprince@gmail.com>
2022-10-21 12:21:50 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,46 +1,20 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import AddUser from './components/AddUser/AddUser';
+import Home from './components/Home/Home';
+import UpdateUser from './components/UpdateUser/UpdateUser';
+
 
 function App() {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    fetch('http://localhost:5000/users')
-      .then(res => res.json())
-      .then(data => setUsers(data));
-  }, [])
-  const handleAddUser = event => {
-    event.preventDefault();
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-    const user = { name, email };
-    fetch('http://localhost:5000/user', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(user)
-    })
-      .then(res => res.json())
-      .then(data => {
-        const newUsers = [...users, data];
-        setUsers(newUsers);
-        console.log(data);
-      })
-  }
-
 
 
   return (
     <div className="App">
-      <h1>My own data {users.length}</h1>
-      <form onSubmit={handleAddUser}>
-        <input type="text" name="name" placeholder='Name' required />
-        <input type="text" name="email" placeholder='Email' required />
-        <input type="submit" value="Add User" />
-      </form>
-      <ul>
-        {users.map(user => <li key={user.id}>id:{user.id} name:{user.name} email:{user.email}</li>)}
-      </ul>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="user/add" element={<AddUser></AddUser>}></Route>
+        <Route path='/update/:id' element={<UpdateUser></UpdateUser>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/AddUser/AddUser.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const AddUser = () => {
+    const handleAddUser = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const user = { name, email };
+
+        fetch('http://localhost:5000/user', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('success', data);
+                alert('users added successfully!!!')
+                event.target.reset();
+            })
+    }
+    return (
+        <div>
+            <h2>Pleace add a new usesr</h2>
+            <form onSubmit={handleAddUser}>
+                <input type="text" name="name" placeholder='Name' required />
+                <br />
+                <input type="email" name="email" placeholder='Email' required />
+                <br />
+                <input type="submit" vlaue="Add User" />
+            </form>
+        </div>
+    );
+};
+
+export default AddUser;
